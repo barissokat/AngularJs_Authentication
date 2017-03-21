ef0b6bedd5f827aef043725d0172e875e6f8ca8a
bsokat <barissokat@gmail.com>
2017-03-21 18:17:34 +0300

    Add Owin Startup Class

---
+++ AngularJs_Authentication.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularJs_Authentication.API.WebApiApplication" Language="C#" %>
---
+++ AngularJs_Authentication/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AngularJs_Authentication.Startup))]
+namespace AngularJs_Authentication
+{
+    public class Startup
+    {
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                HttpConfiguration config = new HttpConfiguration();
+                WebApiConfig.Register(config);
+                app.UseWebApi(config);
+            }
+
+        }
+    }
+}
