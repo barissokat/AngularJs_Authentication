17553034c68d440d3a3fd1c6f05a52c135ac2960
bsokat <barissokat@gmail.com>
2017-03-29 08:21:18 +0300

    Add the new Database Entities

--- AngularJs_Authentication.API/AuthContext.cs
+++ AngularJs_Authentication.API/AuthContext.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using AngularJs_Authentication.API.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -13,5 +15,7 @@ namespace AngularJs_Authentication.API
         {
 
         }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
---
+++ AngularJs_Authentication.API/Models/Enum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJs_Authentication.API.Models
+{
+    public enum ApplicationTypes
+    {
+        JavaScript = 0,
+        NativeConfidential = 1
+    };
+}
