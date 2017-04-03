d6b994d7ddcd09c7c307abd6ae4a3502b03df5e3
bsokat <barissokat@gmail.com>
2017-04-03 08:13:09 +0300

    Update Facebook authentication provider

--- AngularJs_Authentication.API/Providers/FacebookAuthProvider.cs
+++ AngularJs_Authentication.API/Providers/FacebookAuthProvider.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.Owin.Security.Facebook;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace AngularJs_Authentication.API.Providers
 {
-    public class FacebookAuthProvider
+    public class FacebookAuthProvider : FacebookAuthenticationProvider
     {
+        public override Task Authenticated(FacebookAuthenticatedContext context)
+        {
+            context.Identity.AddClaim(new Claim("ExternalAccessToken", context.AccessToken));
+            return Task.FromResult<object>(null);
+        }
     }
 }
