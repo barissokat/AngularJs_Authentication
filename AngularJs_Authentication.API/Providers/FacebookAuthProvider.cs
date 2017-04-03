59a8b3cf8d2e1cc1e26d3219282f7b6984534837
bsokat <barissokat@gmail.com>
2017-04-03 08:11:50 +0300

    Add Facebook authentication providers

---
+++ AngularJs_Authentication.API/Providers/FacebookAuthProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJs_Authentication.API.Providers
+{
+    public class FacebookAuthProvider
+    {
+    }
+}
